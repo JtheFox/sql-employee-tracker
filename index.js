f918aa58d4ae247d2656537c61815e94e834f0e1
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-23 12:21:13 -0400

    add main process function

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const table = require('console.table');
+const prompts = require('./utils/prompts');
+const queries = require('./utils/queries')
 
+async function main() {
+    const choice = await prompts.menu();
+    if (choice === 'quit') return;
+    else if (!prompts[`${choice}`]) await queries[`${choice}`]();
+    else {
+        const addVal = await prompts[`${choice}`]();
+        await queries[`${choice}`](addVal);
+    }
+
+    main();
+}
+
+main();
