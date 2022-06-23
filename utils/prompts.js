bfbe3852f34ce359100953e9e13330de3c02c77c
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-23 11:55:13 -0400

    add menu and subprompt

--- utils/prompts.js
+++ utils/prompts.js
@@ -1,1 +1,29 @@
 const inquirer = require('inquirer');
+
+exports.menu = async () => {
+    const { menuSelect } = await inquirer.prompt({
+        type: 'list',
+        name: 'menuSelect',
+        message: 'What would you like to do?',
+        choices: [
+            { name: 'View All Departments', value: 'viewDepartments' },
+            { name: 'Add Department', value: 'addDepartment' },
+            { name: 'View All Employees', value: 'viewEmployees' },
+            { name: 'Add Employee', value: 'addEmployee' },
+            { name: 'View All Roles', value: 'viewRoles' },
+            { name: 'Add Role', value: 'addRole' },
+            { name: 'Update Employee Role', value: 'updateEmployee' },
+            { name: 'Quit', value: 'quit' }
+        ]
+    });
+    return menuSelect;
+}
+
+exports.addDepartment = async () => {
+    const { departmentName } = await inquirer.prompt({
+        type: 'input',
+        name: 'departmentName',
+        message: 'What is the name of the department?'
+    });
+    return departmentName;
+}
