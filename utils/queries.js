0e049d86680c5615139732d54dede726e2a7bf4a
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-26 14:38:27 -0400

    add view roles and employees queries

--- utils/queries.js
+++ utils/queries.js
@@ -8,11 +8,39 @@ const db = mysql.createConnection({
     database: 'employees_db'
 });
 
+const log = (err, result) => {
+    console.log('\n');
+    if (err) console.error(err);
+    else console.table(result);
+    console.log('Use up or down to return to menu')
+}
+
 exports.viewDepartments = () => {
     db.query('SELECT * FROM departments', (err, result) => {
-        console.log('\n');
-        if (err) console.error(err);
-        console.table(result);
-        console.log('Use up or down to return to menu')
+        log(err, result);
+    });
+}
+
+exports.viewRoles = () => {
+    db.query(
+    `SELECT roles.id, roles.title, departments.name AS department, roles.salary
+    FROM roles
+    LEFT JOIN departments ON (departments.id = roles.department_id)
+    ORDER BY roles.id;`, 
+    (err, result) => {
+        log(err, result);
+    });
+}
+
+exports.viewEmployees = () => {
+    db.query(
+    `SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employees
+    LEFT JOIN employees manager on manager.id = employees.manager_id
+    INNER JOIN roles ON (roles.id = employees.role_id)
+    INNER JOIN departments ON (departments.id = roles.department_id)
+    ORDER BY employees.id;`, 
+    (err, result) => {
+        log(err, result);
     });
 }
