e2305c4ff65f1f721c00ffa2a37be4b014ebdfc5
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-26 13:37:20 -0400

    add initial query

--- utils/queries.js
+++ utils/queries.js
@@ -1,1 +1,20 @@
 const mysql = require ('mysql2');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+exports.viewDepartments = async () => {
+    db.query('SELECT * FROM DEPARTMENTS', (err, result) => {
+        if (err) console.error(err);
+        console.log(result);
+    });
+}
