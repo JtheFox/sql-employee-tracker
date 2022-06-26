d885a9a9db8f0243a772a1eab2ebd7004ad747dc
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-26 14:25:48 -0400

    update quit function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const queries = require('./utils/queries');
 
 async function main() {
     const choice = await prompts.menu();
-    if (choice === 'quit') return;
+    if (choice === 'quit') process.exit(0);
     else if (!prompts[`${choice}`]) await queries[`${choice}`]();
     else {
         const addVal = await prompts[`${choice}`]();
